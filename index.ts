7d2c0fb765d391ba2d698fa232a284cfa4e9f9c3
Arela-Cristina <instagrandine@gmail.com>
2025-04-18 19:05:42 +0200

    Millestone 3 work done

--- index.ts
+++ index.ts
@@ -21,7 +21,6 @@ type Person = {
 
 // 📌 Milestone 2
 // Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
-
 // most_famous_movies: una tuple di 3 stringhe
 // awards: una stringa
 // nationality: una stringa tra un insieme definito di valori.
@@ -34,3 +33,55 @@ type Actress = Person & {
     awards: string,
     nationality: Nationality,
 }
+
+// 📌 Milestone 3
+// Crea una funzione getActress che, dato un id, effettua una chiamata a:
+// GET https://boolean-spec-frontend.vercel.app/freetestapi/actresses/:id
+// La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+// Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta. fatto
+
+
+
+// type guard :v 
+function isActress(object: unknown): object is Actress {
+
+    const validNationality = ['American', 'British', 'Australian', 'Israeli-American', 'South African', 'French', 'Indian', 'Israeli', 'Spanish', 'South-Korean', 'Chinese']
+
+    return (
+        typeof object === "object" && object !== null &&
+        'id' in object && typeof object.id === 'number' &&
+        'name' in object && typeof object.name === 'string' &&
+        'birth_year' in object && typeof object.birth_year === 'number' &&
+        'death_year' in object && (typeof object.death_year === 'number' || typeof object.death_year === 'undefined') &&
+        'biography' in object && typeof object.biography === 'string' &&
+        'image' in object && typeof object.image === 'string' &&
+        'most_famous_movies' in object &&
+        object.most_famous_movies instanceof Array &&
+        object.most_famous_movies.length === 3 &&
+        object.most_famous_movies.every(el => typeof el === 'string') &&
+        'awards' in object && typeof object.awards === 'string' &&
+        'nationality' in object && typeof object.nationality === 'string' &&
+        validNationality.includes(object.nationality)
+    )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+    try {
+        const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`);
+        const data: unknown = await response.json()
+        if (!isActress(data)) {
+            throw new Error('Data non e un oggetto')
+        }
+        return data
+
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log('Errore durante la chiamata', error)
+        } else {
+            console.log(error)
+        }
+        return null
+    }
+}
+
+
